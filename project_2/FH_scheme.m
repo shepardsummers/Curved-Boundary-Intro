9bdf30e1a014dc5dc6b4ec0d7d7a5a64e86b44a4
Shepard Summers <115957978+shepardsummers@users.noreply.github.com>
2025-05-21 16:11:12 -0400

    Before lots of changes

---
+++ project_2/FH_scheme.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = FH_scheme(inputArg1,inputArg2)
+%FH_SCHEME Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
